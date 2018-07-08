bd3c6afe32cdff4a138ed8026ecc2747380f2bdc
Xiake <xiake@ansebbian.com>
2018-07-08 00:45:28 -0500

    Fixing HSX

---
+++ WarpWeb 2.0/database/schema/WarpSec.sql
@@ -0,0 +1,31 @@
+DROP DATABASE IF EXISTS WarpSec_CN;
+CREATE DATABASE WarpSec_CN CHARACTER SET utf8 COLLATE utf8_general_ci;
+
+CREATE TABLE Parameters (
+	ID 									BIGINT PRIMARY KEY AUTO_INCREMENT,
+    RecordStatusID						BIGINT DEFAULT 10, 
+    CreateDate 							DATETIME DEFAULT CURRENT_TIMESTAMP,
+    AuID 								BIGINT DEFAULT -1,
+	IuID 								BIGINT DEFAULT -1,
+	LastModifyDate 						DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+	LastAuID 							BIGINT DEFAULT -1,
+	LastIuID 							BIGINT DEFAULT -1,
+    WarpKey								NVARCHAR(255), 
+    WarpValue							NVARCHAR(2000)
+);
+
+
+
+/* 
+
+Environments
+
+	TEST_CN
+    TEST_US
+    UAT_CN
+    UAT_US
+    PROD_CN
+    PROD_US
+
+
+*/
